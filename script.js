51de0ba394f759f6e692bd26a8482ed83ffd2c89
sushant2000 <sushantmawar10@gmail.com>
2021-12-24 15:39:00 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 let rows = 100;
 let col = 26;
 
+let addressColCont = document.querySeelctor(".address-col-cont");
+for(let i = 0; i < rows; i++){
+    let addressCol = document.createElement("div");
+    addressCol.innerText = i+1;
+    addressColCont.appendChild(addressCol);
+}
