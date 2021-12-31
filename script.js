00a233c9aae35d08f797d91bf3eed6db54fbce25
sushant2000 <sushantmawar10@gmail.com>
2022-01-01 00:31:38 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+/*Generating Rows and columns Name start*/ 
 let rows = 100;
 let col = 26;
 
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
 for(let i = 0; i < rows; i++){
     let addressCol = document.createElement("div");
     addressCol.setAttribute("class" , "address-col");
@@ -13,6 +15,23 @@ for(let i = 0; i < rows; i++){
 for( let i = 0; i < col; i++){
     let addressRow = document.createElement("div");
     addressRow.setAttribute("class" , "address-row");
-    addressRow.innerText = i+1;
+    addressRow.innerText = String.fromCharCode(65 + i);
     addressRowCont.appendChild(addressRow);
 }
+/*Generating Rows and columns Name end*/
+
+
+/** Grid Generating start*/
+
+for(let i = 0; i< rows; i++){
+    let rowCont = document.createElement("div");
+    rowCont.setAttribute("class" , "row-cont");
+    for(let j = 0; j < col; j++){
+        let cell = document.createElement("div");
+        cell.setAttribute("class" , "cell");
+        cell.setAttribute("contenteditable" , "true");
+        rowCont.appendChild(cell);
+    }
+    cellsCont.appendChild(rowCont);
+}
+/** Grid Generating end*/
