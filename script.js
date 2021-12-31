fec2c96f8589fa81fa04e8d88f2b6b9c46cee3a7
sushant2000 <sushantmawar10@gmail.com>
2021-12-31 10:02:25 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 let rows = 100;
 let col = 26;
 
-let addressColCont = document.querySeelctor(".address-col-cont");
+let addressColCont = document.querySelector(".address-col-cont");
+let addressRowCont = document.querySelector(".address-row-cont");
 for(let i = 0; i < rows; i++){
     let addressCol = document.createElement("div");
+    addressCol.setAttribute("class" , "address-col");
     addressCol.innerText = i+1;
     addressColCont.appendChild(addressCol);
+}
+
+for( let i = 0; i < col; i++){
+    let addressRow = document.createElement("div");
+    addressRow.setAttribute("class" , "address-row");
+    addressRow.innerText = i+1;
+    addressColCont.appendChild(addressRow);
 }
