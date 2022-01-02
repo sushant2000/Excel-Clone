e5cf1e92fdecb085c05dc190f6f239c75ca416a3
sushant2000 <sushantmawar10@gmail.com>
2022-01-02 20:21:54 +0530

    update

--- formula.js
+++ formula.js
@@ -17,16 +17,18 @@ formulaBar.addEventListener("keydown", (e) => {
   let inputFormula = formulaBar.value;
   if (e.key === "Enter" && inputFormula) { //Enter key must be press + formula bar should contain some value too
     let evaluatedValue = evaluateFormula(inputFormula);
+
+    //to update UI and cellProp in DB
     setCellUIAndCellProp(evaluatedValue, inputFormula);
   }
-});
+})
 
 function evaluateFormula(formula) {
     let encodedFormula = formula.split(" ");
     for(let i = 0; i<encodedFormula.length; i++){
         let asciiValue = encodedFormula[i].charCodeAt(0);
         if(asciiValue >= 65 && asciiValue <= 90){
-            let [cell , cellProp] = getCellAndCellProp(encodedFormula[i]);
+            let [cell, cellProp] = getCellAndCellProp(encodedFormula[i]);
             encodedFormula[i] =  cellProp.value;
         }
     }
