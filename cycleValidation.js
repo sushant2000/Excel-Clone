1dd41dffe87d1a14772d102d62767f1fb90ff237
sushant2000 <sushantmawar10@gmail.com>
2022-01-03 10:46:57 +0530

    update

--- cycleValidation.js
+++ cycleValidation.js
@@ -1,10 +1,10 @@
-//Storage -> 2D Matrix (Basic )
+//Storage -> 2D Matrix (Basic ) overall it is represent as 3D Matrix
 let graphComponentMatrix = [];
 
 for(let i = 0; i< rows; i++){
     let row = []
     for(let j = 0; j < col; j++){
-      //Why Array -> More than one child relation(dependency) so i.e why array
+      //Why Array -> More than one child relation(dependency) so i.e why use array
         row.push([]) ;
     }
     graphComponentMatrix.push(row);
